fb7aea5339af980770cc66e452d6a9f919244003
jnadzieja <joshua.nadzieja@gmail.com>
2018-10-01 12:18:13 -0400

    WE DID IT BOIIIII

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -81,6 +81,7 @@ function pullUserData() {
         favLink.text(favKeys[i]);
         favLink.addClass("fav-links");
         favLink.attr("href", link);
+        favLink.addClass("fav-list")
 
         $(".user-list").append(favLink);
         $(".user-list").append("<br> <br>");
@@ -213,14 +214,14 @@ function pushFavorite() {
 $("#login-btn").on("click", function() {
   $("#mainMenu").animate({
     width: "300px"
-  });
+  }, 1);
   $("#login-btn").toggleClass("hide");
 });
 //on click handler for closing menu
 $("#close").on("click", function() {
   $("#mainMenu").animate({
     width: "0px"
-  });
+  }, 1);
   $("#login-btn").toggleClass("hide");
 });
 //on click handler for search bar on navigation page
@@ -279,6 +280,7 @@ $(document).on("click", ".show-link", function() {
   $("#detailsTitle").addClass("text-center")
   $("#detailsTitle").addClass("mx-auto")
   $("#buyTickets").attr("href", eventTickets);
+  $("#buyTickets").attr("target", "_blank");
   $("#infoDiv").removeClass("hide");
 });
 //on click handler for adding user favorite
